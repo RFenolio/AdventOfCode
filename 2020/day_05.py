00be4adc9ff7f895d9f6a6bcd9ccc83724977c8f
Ron <rfenolio@coursera.org>
2020-12-04 23:40:16 -0800

    simplify day 5

--- 2020/day_05.py
+++ 2020/day_05.py
@@ -2,35 +2,9 @@
 with open("day_05.txt") as f:
 	seats = f.read().splitlines()
 
-def calculate_row(row_code: str) -> int:
-	seat = list(range(128))
-	for char in row_code:
-		mid = len(seat)//2
-		if char == "F":
-			seat = seat[:mid]
-		else: # char == "B"
-			seat = seat[mid:]
-	return seat[0]
-
-assert calculate_row("FBFBBFF") == 44
-
-def calculate_col(col_code: str) -> int:
-	col = list(range(8))
-	for char in col_code:
-		mid = len(col)//2
-		if char == "L":
-			col = col[:mid]
-		else: # char == "B"
-			col = col[mid:]
-	return col[0]
-
-assert calculate_col("RLR") == 5
-
 def get_seat_id(seat_code: str) -> int:
-	row_code = seat_code[:7]
-	col_code = seat_code[7:]
-	return (calculate_row(row_code) * 8) +calculate_col(col_code)
-
+	"""It's just a binary string"""
+	return int(seat_code.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2)
 
 assert get_seat_id("BFFFBBFRRR") == 567
 assert get_seat_id("FFFBBBFRRR") == 119
