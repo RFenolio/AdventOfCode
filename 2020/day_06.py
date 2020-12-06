ad16cc8d3dad9c11c9756dbb51f4f4880f8bff98
Ron <rfenolio@coursera.org>
2020-12-06 00:45:25 -0800

    play code golf

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -1,8 +1,6 @@
 from string import ascii_lowercase
 
 with open("day_06.txt") as f:
-	groups = [group_str.split("\n") for group_str in f.read().split("\n\n")]
-
-print("Part 1:", sum((len(x) for x in [set().union(*(set(form) for form in group))for group in groups])))
-
-print("Part 2:", sum((len(x) for x in [set(ascii_lowercase).intersection(*(set(form) for form in group))for group in groups])))
+	print("Part 1:", sum((len(x) for x in [set().union(*(set(form) for form in group))for group in (group_str.split("\n") for group_str in f.read().split("\n\n"))])))
+with open("day_06.txt") as f:
+	print("Part 2:", sum((len(x) for x in [set(ascii_lowercase).intersection(*(set(form) for form in group))for group in (group_str.split("\n") for group_str in f.read().split("\n\n"))])))
