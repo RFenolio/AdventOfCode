c638d6e2fe495cfe594b12857b8b292fcd9c319b
Ron <rfenolio@coursera.org>
2021-12-05 21:46:45 -0800

    2021 - day 06

---
+++ 2021/day06.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from typing import List
+"""
+Get data 
+"""
+input_file = os.path.join(sys.path[0], "day06_input.txt")
+with open(input_file) as f:
+    puzzle_input = [int(val) for val in f.read().split(",")]
+
+"""
+Part 1
+"""
+def calculate_fish(fish_ages: List[int], num_days: int):
+    # calculate number of fish of each age
+    fish_counts: List[int] = [0] * 9
+    for fish in fish_ages:
+        fish_counts[fish] += 1
+    
+    # model fish for a number of days
+    for _ in range(num_days):
+        today = fish_counts.pop(0)
+        fish_counts.append(0)
+        fish_counts[6] += today
+        fish_counts[8] += today
+    
+    return sum(fish_counts)
+    
+test_fish = [3,4,3,1,2]
+assert calculate_fish(test_fish, 80) == 5934
+print("Part 1:", calculate_fish(puzzle_input, 80))
+
+
+"""
+Part 2
+"""
+assert calculate_fish(test_fish, 256) == 26984457539
+print("Part 1:", calculate_fish(puzzle_input, 256))
