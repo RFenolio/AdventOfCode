ec01576de9952cb865d9cc355f96157bae059471
Ron <rfenolio@coursera.org>
2021-12-06 22:47:14 -0800

    day 07

---
+++ 2021/day07.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+"""
+Get data 
+"""
+input_file = os.path.join(sys.path[0], "day07_input.txt")
+with open(input_file) as f:
+    puzzle_input = [int(val) for val in f.read().split(",")]
+
+test_input = [16,1,2,0,4,2,7,1,2,14]
+
+def crab_sum(dist):
+    return (dist * (dist + 1)) // 2
+
+def find_straight_fuel(crabs, idx):
+    return sum(abs(x - idx) for x in crabs)
+
+def find_crab_dist_fuel(crabs, idx):
+    return sum(crab_sum(abs(x - idx)) for x in crabs)
+
+def find_fuel(f, crabs):
+    return min(f(crabs, idx) for idx in range(min(crabs), max(crabs)+1))
+
+assert find_fuel(find_straight_fuel, test_input) == 37
+print("Part 1:", find_fuel(find_straight_fuel, puzzle_input))
+
+assert find_fuel(find_crab_dist_fuel, test_input) == 168
+print("Part 2:", find_fuel(find_crab_dist_fuel, puzzle_input))
